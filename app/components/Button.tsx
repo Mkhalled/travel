e712500d3dc7b1900df07be6bef3b1ece28bedfb
Mkhalled <khalled.men@gmail.com>
2023-10-22 10:13:26 +0200

    :sparcles: added GetApp

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
-import React from "react";
 
-interface Props {
+type ButtonProps = {
+  type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
-  type: "button" | "submit";
-}
-function Button({ title, icon, variant, type }: Props) {
+  variant: string;
+  full?: boolean;
+};
+
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
-}
+};
 
 export default Button;
